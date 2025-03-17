9b573fc0b00804cf6549a2a163d878a98f0d4648
shaikat020 <tahsin.azad.skt@gmail.com>
2025-03-17 20:02:19 +0600

    Fixing authentication

--- services/views.py
+++ services/views.py
@@ -28,9 +28,9 @@ def index(request):
 # ---------------------
 def login_view(request):
     if request.method == 'POST':
-        username = request.POST.get('username')
+        email = request.POST.get('email')
         password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
+        user = authenticate(request, email=email, password=password)
         if user is not None:
             login(request, user)
             return redirect('index')
